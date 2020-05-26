3b693a6ac8a509fad463ed6880ca793918a60dcb
Anne Williams <mawilliams098@gmail.com>
2020-05-26 15:19:36 -0400

    rename project to mysite

--- polls/models.py
+++ polls/models.py
@@ -13,7 +13,8 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-
+# Finally, note a relationship is defined, using ForeignKey. 
+# That tells Django each Choice is related to a single Question.
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
