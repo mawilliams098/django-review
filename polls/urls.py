919e8f803cd3853ffa4b472b0b28af3564f5ed53
Anne Williams <mawilliams098@gmail.com>
2020-05-28 19:58:16 -0400

    urls update

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,20 @@ from django.urls import path
 
 from . import views
 
+"""
+To get from a URL to a view, Django uses what are known as ‘URLconfs’. 
+A URLconf maps URL patterns to views.
+This tutorial provides basic instruction in the use of URLconfs, 
+and you can refer to URL dispatcher for more information.
+"""
+
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
+from .models import Question
+
 
 # request / response objects
 """
@@ -12,8 +14,28 @@ Then Django loads the appropriate view, passing the HttpRequest
 as the first argument to the view function. 
 
 Each view is responsible for returning an HttpResponse object.
+
+Each view is responsible for doing one of two things: returning an HttpResponse object 
+containing the content for the requested page, or raising an exception such as Http404. 
+The rest is up to you.
 """
 
 # to call the view, map it to a url in polls/urls.py
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context) # render returns an HttpResponse
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+    
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
